4f23be93574e156ea5382a69e8fa332fc6065d98
DPU J S <DPU J S@DESKTOP-12TU7QI>
2020-10-09 19:25:57 +0530

    Add project files.

---
+++ LinqProject/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace LinqProject
+{
+    public static class StringExtensions
+    {
+       public static string ConvertToTitleCase(string source)
+        {
+            CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
+            TextInfo textInfo = cultureInfo.TextInfo;
+            return textInfo.ToTitleCase(source);
+        }
+    }
+}
---
+++ LinqProjectTests/StringExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinqProject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqProject.Tests
+{
+    [TestClass()]
+    public class StringExtensionsTests
+    {
+        public TestContext TestContext { get; set; }
+        [TestMethod()]
+        public void ConvertToTitleCaseTest()
+        {
+            var source = "The return of the king";
+            var expected = "The Return of the king";
+            var result = StringExtensions.ConvertToTitleCase(source);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
